c3a5913743168ecbff24b34b1dca919aa9adc9a1
jstudenski <jstudenski@gmail.com>
2018-12-16 17:38:06 -0600

    data upload utility

--- src/App.js
+++ src/App.js
@@ -6,35 +6,25 @@ import Processor from './components/processor'
 import './App.css';
 
 class App extends Component {
-
+  constructor ( props ) {
+    super(props);
+    this.state = {computers: {}};
+  }
   componentDidMount = async () => {
-    const snapshot = await firestore.collection('computers').get();
-    // console.log(computers);
-    // computers.forEach(doc => {
-    //   const id = doc.id;
-    //   const data = doc.data();
-    //   console.log({ id, data });
-    // });
-
+    const snapshot = await firestore.collection('data').get();
     const computers = snapshot.docs.map(computer => ({ id: computer.id, ...computer.data() }));
     this.setState({ computers });
   }
 
   render() {
+    const { computers } = this.state
     const border = {
       borderTop: '1px solid #d6d6d6',
       width: 400,
     }
-
-    console.log(this.state)
-    // const sidebar = {
-    //   backgroundColor: '#f7f7f7',
-    //   width: 200,
-    // }
-
+    // console.log(this.state.computers)
     return (
       <main>
-        {/* <aside style={sidebar} /> */}
         <section>
           Hello
         </section>
