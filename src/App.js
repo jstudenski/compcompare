7c8163e568c19e587080c357cb25c75e0fa2f7c0
jstudenski <jstudenski@gmail.com>
2018-12-23 18:36:43 -0600

    col span

--- src/App.js
+++ src/App.js
@@ -18,21 +18,20 @@ class App extends Component {
 
   render() {
     const { computers } = this.state
-
     const grey = '1px solid hsl(202,10%,88%)'
     const border = {
       borderTop: '1px solid #d6d6d6',
       width: 400,
     }
-
     const grid = {
       display: 'flex',
     }
-
     const cell = {
       borderBottom: grey,
       borderLeft: grey,
       padding: 6,
+      height: 200,
+      overflow: 'hidden',
     }
     const section = {
       borderTop: grey,
@@ -64,6 +63,40 @@ class App extends Component {
           <div style={border} />
           <Processor/>
           <div style={border} />
+        </section>
+        <section>
+        <div class="container">
+          <div class="column">
+            <div class="button rowspan">
+              Column 1 - rowspan
+            </div>
+            <div class="button">
+              Column 7
+            </div>
+          </div>
+          <div class="column">
+            <div class="button">
+              Column 2
+            </div>
+            <div class="button">
+              Column 5
+            </div>
+            <div class="button">
+              Column 8
+            </div>
+          </div>
+          <div class="column">
+            <div class="button">
+              Column 3
+            </div>
+            <div class="button">
+              Column 6
+            </div>
+            <div class="button">
+              Column 9
+            </div>
+          </div>
+        </div>
         </section>
       </main>
     );
